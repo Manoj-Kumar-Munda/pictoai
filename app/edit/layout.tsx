c4411f1fc3ff6fab5be4c1ccf191c41136528af9
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-03 22:45:29 +0530

    Refactor ImageEditorLayout to use a div wrapper for improved layout structure

--- app/edit/layout.tsx
+++ app/edit/layout.tsx
@@ -3,16 +3,16 @@ import Header from "./_components/header";
 
 const ImageEditorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="h-svh overflow-hidden">
       <Header />
 
-      <main className="h-svh w-full relative pt-20  text-black flex ">
+      <main className="h-full w-full relative pt-20 text-black flex items-start">
         <EditSidebar />
         <div className="flex-1 h-full w-full flex justify-center p-4 bg-gray-200">
           {children}
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
