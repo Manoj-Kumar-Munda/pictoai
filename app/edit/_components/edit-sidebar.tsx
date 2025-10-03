67c1b8ad034ddb281bfcad1d015f9ff1b6f56263
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-03 22:45:50 +0530

    Add AI effects to EditSidebar with effect cards

--- app/edit/_components/edit-sidebar.tsx
+++ app/edit/_components/edit-sidebar.tsx
@@ -1,8 +1,30 @@
+import effect1 from "@/app/assets/ai_effects/lightings/black-white-studio.png";
+import { StaticImageData } from "next/image";
+import AIEffectCard from "@/components/effect-card";
+import effect2 from "@/app/assets/ai_effects/lightings/low-key.png";
+
+const effects: { name: string; demo: StaticImageData }[] = [
+  {
+    name: "Studio",
+    demo: effect1,
+  },
+  {
+    name: "Low-key",
+    demo: effect2,
+  },
+];
+
 const EditSidebar = () => {
   return (
-    <aside className="relative left-0 bottom-0 h-full bg-black  w-1/6  text-white">
-      <div className="p-4">
-        <h2 className="text-lg font-semibold">AI Effects</h2>
+    <aside className="h-full bg-black px-4  text-white">
+      <h2 className="text-lg font-semibold">AI Effects</h2>
+
+      <div className="h-full overflow-y-auto">
+        <div className="grid grid-cols-3 gap-y-4 gap-x-3.5">
+          {effects.map((effect) => (
+            <AIEffectCard key={effect.name} {...effect} />
+          ))}
+        </div>
       </div>
     </aside>
   );
