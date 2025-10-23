cf0b676a1665458f9741b3eb16a8f45185d23de5
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-23 20:25:04 +0530

    refactor: Header is moved to components and Editlayout updated

--- app/edit/layout.tsx
+++ app/edit/layout.tsx
@@ -1,11 +1,8 @@
 import EditSidebar from "./_components/edit-sidebar";
-import Header from "./_components/header";
 
 const ImageEditorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-svh overflow-hidden">
-      <Header />
-
       <main className="h-full w-full relative pt-20 text-black flex items-start">
         <EditSidebar />
         <div className="flex-1 h-full w-full flex justify-center p-4 bg-gray-200">
