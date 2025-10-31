ef4e2bf5009395d974e406e015ed50fd723512f3
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-31 18:04:11 +0530

    feat: add logo image and update header component for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </head>
       <body className={`antialiased ${inter.variable} `}>
         <div className="min-h-svh">
           <Header />
--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
+import logo from "@/public/logo.png";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between p-4 bg-black  text-white fixed top-0 left-0 right-0 z-50 h-20">
       {/*logo */}
-      <Link href="/">
+      <Link href="/" className="flex gap-2 items-center">
+        <Image src={logo} alt="logo" width={48} height={48} />
         <span>Picto AI</span>
       </Link>
 
