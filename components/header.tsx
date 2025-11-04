f11a38fcad2de16249960299aa2e6bec0fbed2da
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-11-04 22:20:47 +0530

    refactor: update header styles and clean up unused imports

--- components/header.tsx
+++ components/header.tsx
@@ -2,29 +2,26 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
-import logo from "@/public/logo.png";
-import Image from "next/image";
-import { ThemeToggle } from "./theme-toggle";
 import { cn } from "@/lib/utils";
 
 const Header = () => {
   return (
     <header
       className={cn(
-        "flex items-center justify-between p-4 bg-white/30 backdrop-blur-md text-neutral-800 dark:text-black fixed top-4 left-4 right-4 rounded-full z-50 h-16"
+        "flex items-center justify-between p-4  text-neutral-800 dark:text-black fixed top-0 left-0 right-0 rounded-none z-50 h-16 lg:inset-x-8 xl:inset-x-12"
       )}
     >
       {/*logo */}
       <Link href="/" className="flex gap-2 items-center">
         {/* <Image src={logo} alt="logo" width={48} height={48} /> */}
-        <span className="font-bold font-sans ">Picto AI</span>
+        <span className="font-semibold text-lg font-sans text-white">
+          Picto AI
+        </span>
       </Link>
-
-      {/* navigation */}
 
       {/* user actions or profile avatar */}
       <div className="flex items-center gap-3">
-        <ThemeToggle />
+        {/* <ThemeToggle /> */}
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
