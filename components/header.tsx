c510d1c97b9e8f4fc3faebe4eacea024917f9c45
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-11-04 22:50:26 +0530

    refactor: update header component

--- components/header.tsx
+++ components/header.tsx
@@ -3,18 +3,28 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const isHome = pathname === "/";
   return (
     <header
       className={cn(
-        "flex items-center justify-between p-4  text-neutral-800 dark:text-black fixed top-0 left-0 right-0 rounded-none z-50 h-16 lg:inset-x-8 xl:inset-x-12"
+        "flex items-center justify-between p-4 fixed top-0 inset-x-0 rounded-none z-50 h-16 lg:inset-x-8 xl:inset-x-12",
+        !isHome && "xl:inset-x-2 lg:inset-x-2"
       )}
     >
       {/*logo */}
       <Link href="/" className="flex gap-2 items-center">
         {/* <Image src={logo} alt="logo" width={48} height={48} /> */}
-        <span className="font-semibold text-lg font-sans text-white">
+        <span
+          className={cn(
+            "font-semibold text-lg font-sans text-white",
+            !isHome && " text-neutral-800"
+          )}
+        >
           Picto AI
         </span>
       </Link>
