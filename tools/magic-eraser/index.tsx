5def0f389b51e67777fa2f7b9de0ca370cc8473e
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-11-13 22:15:28 +0530

    feat: add Slider component for brush size adjustment in Magic Eraser

--- tools/magic-eraser/index.tsx
+++ tools/magic-eraser/index.tsx
@@ -1,3 +1,4 @@
+import { Slider } from "@/components/ui/slider";
 import { MAX_BRUSH_SIZE, MIN_BRUSH_SIZE } from "@/constants/tools";
 import useApplyEffect from "@/hooks/useSendChat";
 import useEditingStore from "@/store/editing-store";
@@ -16,28 +17,33 @@ const MagicEraserPopup = () => {
 
   return (
     <div className="absolute top-4 left-4 bg-gray-100 shadow p-4 z-10 rounded-md max-w-72 ">
-      <h3 className="text-primary font-semibold text-sm">Magic Eraser</h3>
+      <h3 className="text-primary font-semibold text-sm ">Magic Eraser</h3>
       <p className="text-[10px] text-neutral-500 font-medium">
         {currentTool?.description}
       </p>
 
       <div className="flex flex-col  pt-2">
-        <label className="block mt-2 text-xs text-neutral-500 font-medium ">
+        <label className="block mt-2 text-xs text-neutral-700 font-medium ">
           Brush size
         </label>
-        <input
-          type="range"
+
+        <Slider
+          defaultValue={[brushSize]}
+          onValueChange={(value) => setBrushSize(value[0])}
           min={MIN_BRUSH_SIZE}
           max={MAX_BRUSH_SIZE}
-          value={brushSize}
-          onChange={(e) => setBrushSize(Number(e.target.value))}
-          className="w-full "
+          step={5}
+          className="w-full mt-2"
+          trackClassName="bg-gray-300"
+          thumbClassName="bg-gray-100 border-gray-100 shadow-md border border-neutral-400"
         />
-        <span className="ml-auto text-xs font-medium">{brushSize}</span>
+        <span className="ml-auto text-xs font-medium text-neutral-700">
+          {brushSize}
+        </span>
       </div>
 
       <button
-        className="mt-2 flex gap-2 w-full active-effect text-xs font-semibold items-center justify-center py-1.5 rounded text-neutral-700 cursor-pointer hover:brightness-90 transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="mt-2 flex gap-2 w-full bg-primary text-xs font-semibold items-center justify-center py-1.5 rounded text-secondary cursor-pointer hover:brightness-90 transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
         onClick={handleRemove}
         disabled={status === "streaming" || status === "submitted"}
       >
