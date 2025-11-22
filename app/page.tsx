4e18b7d5b11fa8156589665f1e419a136df7611c
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-11-22 10:29:45 +0530

    feat: add gradient utilities and enhance header animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { DraggableEffectsCard } from "@/components/draggable-effects-card";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-between px-4 flex-col pt-20">
-      <div className=" lg:space-y-4 xl:space-y-8">
+    <div className="flex items-center justify-between  flex-col pt-20">
+      <div className=" space-y-2 lg:space-y-4 xl:space-y-8 ">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-balance font-bold text-center tracking-tight leading-tight md:leading-tight lg:leading-16 xl:leading-20">
           Transform your photos
           <br /> with{" "}
@@ -12,9 +12,8 @@ export default function Home() {
           </span>
         </h1>
 
-        <p className="text-center text-neutral-500 font-medium  xl:text-xl">
+        <p className="text-center text-neutral-500 font-medium text-sm  xl:text-lg max-w-xl xl:max-w-2xl mx-auto">
           Unlock professional-grade editing with a single click. Effortlessly
-          <br />
           remove backgrounds, generate stunning outfits, and enhance details
           instantly.
         </p>
--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,40 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { useMotionValueEvent, useScroll, motion } from "motion/react";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { scrollY } = useScroll();
   const pathname = usePathname();
+  const isMobile = useIsMobile();
 
   const isHome = pathname === "/";
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 50) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  });
   return (
-    <header
+    <motion.header
+      animate={{
+        width: isScrolled ? (isMobile ? "90%" : "80%") : "100%",
+        y: isScrolled ? 8 : 0,
+        border: isScrolled ? "1px solid rgba(200,200,200,0.2)" : "none",
+        borderRadius: isScrolled ? "999px" : "0",
+      }}
+      transition={{
+        duration: 0.3,
+        ease: "easeInOut",
+      }}
       className={cn(
-        "max-w-7xl mx-auto flex items-center justify-between px-4 border border-neutral-100 rounded-full py-3 sm:p-4 fixed inset-x-0 z-50 h-14 sm:h-16 top-0 bg-white/30 backdrop-blur-sm",
+        "max-w-7xl mx-auto flex items-center justify-between px-4 md:;pl-8 py-3  fixed inset-x-0 z-50 h-14 sm:h-16 top-0 bg-white/30 dark:bg-white/10 backdrop-blur-sm",
         !isHome && "inset-x-0 xl:inset-x-0 lg:inset-x-0 bg-black text-white"
       )}
     >
@@ -27,12 +52,15 @@ const Header = () => {
       {/* profile avatar */}
       <div className="flex items-center gap-2 sm:gap-3">
         {/* <ThemeToggle /> */}
-        <Avatar className="w-8 h-8 sm:w-10 sm:h-10">
+        {/* <Avatar className="w-8 h-8 sm:w-10 sm:h-10">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        </Avatar> */}
+        <Button className="bg-blue-600 rounded-full text-white">
+          Get started
+        </Button>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
